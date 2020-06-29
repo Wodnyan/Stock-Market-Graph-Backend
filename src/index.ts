9c161e81ba7055659b391336e457bf51863d3af7
Albert Gergo <gergoalbert.gta@gmail.com>
2020-06-29 22:05:46 +0300

    Initial Commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
+    console.log(process.env.API_KEY);
 });
